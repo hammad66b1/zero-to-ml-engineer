60cadf7f934a9051dfd80c3d018911c3ed14a66a
Muhammad Hammad Shafique <hammad66b1@gmail.com>
2026-06-18 11:57:36 +0500

    Add calculator.py - First Simplest Calculator

---
+++ calculator.py
@@ -0,0 +1,10 @@
+number1=20
+number2="16"
+
+print(f"Sum:{number1+int(number2)}")
+
+print(f"Product:{number1*int(number2)}")
+
+print(f"Difference:{number1-int(number2)}")
+
+print(f"Divide:{number1/int(number2)}")
